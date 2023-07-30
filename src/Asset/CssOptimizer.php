5aec5f81f5f8914b02d3e0e6ce1ebbeeb073d71f
Dieter Holvoet <51747-DieterHolvoet@users.noreply.drupalcode.org>
2023-07-30 14:19:25 -0400

    Issue #3371573 by roshni27, empesan, dpineda, DieterHolvoet, lhridley: Call to undefined function file_create_url() in CssOptimizer

--- src/Asset/CssOptimizer.php
+++ src/Asset/CssOptimizer.php
@@ -10,12 +10,29 @@ use Drupal\Core\Asset\CssOptimizer as DrupalCssOptimizer;
  * @codeCoverageIgnore
  */
 class CssOptimizer extends DrupalCssOptimizer {
+  /**
+   * The FileUrl Generator Service.
+   *
+   * @var \Drupal\Core\File\FileUrlGeneratorInterface
+   */
+  protected $fileUrlGenerator;
+
+  /**
+   * Constructs a new CssOptimizer object.
+   *
+   * @param \Drupal\Core\File\FileUrlGeneratorInterface $fileUrlGenerator
+   *   The FileUrl Generator service.
+   */
+  public function __construct(FileUrlGeneratorInterface $fileUrlGenerator) {
+    parent::__construct($fileUrlGenerator);
+    $this->fileUrlGenerator = $fileUrlGenerator;
+  }
 
   /**
    * {@inheritdoc}
    */
-  // phpcs:ignore
-  public function rewriteFileURI($matches) {
+
+  public function rewriteFileURI($matches): string {
     // Prefix with base and remove '../' segments where possible.
     $path = $this->rewriteFileURIBasePath . $matches[1];
     $last = '';
@@ -23,9 +40,8 @@ class CssOptimizer extends DrupalCssOptimizer {
       $last = $path;
       $path = preg_replace('`(^|/)(?!\.\./)([^/]+)/\.\./`', '$1', $path);
     }
-
     // file_url_transform_relative() was removed here.
-    return 'url(' . \Drupal::service('file_url_generator')->generateAbsoluteString($path) . ')';
+    return 'url(' . $this->fileUrlGenerator->generate($path)?->toString() . ')';
   }
 
 }
