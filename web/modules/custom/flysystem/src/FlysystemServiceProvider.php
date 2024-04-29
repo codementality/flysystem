58587a074142dce07e5e09c6b7fd4aa37bf9136a
Lisa Ridley <l.ridley@vardot.com>
2024-04-29 10:11:16 -0400

    Revising service provider class to utilize new FlyStreamWrapper when registering Flysystem stream wrappers as services

--- web/modules/custom/flysystem/src/FlysystemServiceProvider.php
+++ web/modules/custom/flysystem/src/FlysystemServiceProvider.php
@@ -26,10 +26,10 @@ class FlysystemServiceProvider extends ServiceProviderBase {
       if (empty($settings['driver'])) {
         continue;
       }
-      // Registers the FlysystemStreamWrapper.
+      // Registers the FlyStreamWrapper.
       // @todo Revisit if this is needed,
       $container
-        ->register('flysystem_stream_wrapper.' . $scheme, 'Drupal\flysystem\FlyStream\StreamWrapper')
+        ->register('flysystem_stream_wrapper.' . $scheme, 'Drupal\flysystem\FlyStream\FlyStreamWrapper')
         ->addTag('stream_wrapper', ['scheme' => $scheme]);
 
     }
